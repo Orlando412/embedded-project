e4af8c8ef6233634b0aae4f0927b95f15a65210c
Orlando Adams <ojadams@student.fullsail.edu>
2024-08-28 17:25:18 -0400

    corrected the cmake list bug that would not find main cpp file and corrected main cpp to properly run

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,26 @@
 #include "gpio.h"
 #include "trafficLight.h"
 #include "tempControl.h"
+#include <iostream>
+using namespace std;
 
 int main() {
+    cout << "starting embedded sim..." << endl;
     CPU::GPIO gpio(5);  // Assume 5 GPIO pins
     Control::TrafficLightControl trafficLightControl(gpio);
     Control::TemperatureControl temperatureControl(gpio);
 
     RTOS::RTOS rtos;
-
+    cout << "Creating tasks..." << endl;
     // Create tasks for traffic light and temperature control
     rtos.createTask([&]() { trafficLightControl.run(); });
     rtos.createTask([&]() { temperatureControl.run(); });
 
+    cout << "starting rtos..." << endl;
     // Start the RTOS
     rtos.run();
 
+    cout << "rtos finished..." << endl;
+    return 0;
+}
    
